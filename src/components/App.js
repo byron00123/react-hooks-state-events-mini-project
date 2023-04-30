cb97beb8bd3ed0d50803eab3e8d247c2f225a09d
Byron Omondi <omondibyron25@gmail.com>
2023-04-30 17:27:35 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [task, setTask] = useState(TASKS);
+  const [selectedCatBtn, setSelectedCatBtn] = useState("ALL");
+
+  const displayedItem = task.filter((item) => {
+    if (selectedCatBtn === "All") return true;
+    return selectedCatBtn === item.category;
+  });
+
+  const addNewItem = (newItem) => setTask([...task, newItem]);
+
+  const deleteButton = (deleted) =>
+    setTask(task.filter((item) => item.text !== deleted));
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedBtn={setSelectedCatBtn}
+        onBtn={selectedCatBtn}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={addNewItem} />
+      <TaskList tasks={displayedItem} deletedItem={deleteButton} />
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Task() {
+function Task(props) {
+  const { text, category, deletedItem } = props;
+
+  function handleDelete() {
+    deletedItem(text);
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDelete}>
+        X
+      </button>
     </div>
   );
 }
